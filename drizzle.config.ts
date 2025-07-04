ba10dc8a97de3a30c762c493ad4594a99e8cd4ce
venumadhav17 <kalyanamvenumadhav@gmail.com>
2025-07-04 14:51:34 +0530

    Database setup added using drizzle orm

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+  schema: "./config/schema.tsx",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!
+  }
+});
